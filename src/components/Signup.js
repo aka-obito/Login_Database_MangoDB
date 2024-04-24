8b9b3d17eac8105d5de4e06f1f679c732f43d649
Dhawal Turkar <dhawalturkar@gmail.com>
2024-04-24 23:45:24 +0530

    first commit

---
+++ src/components/Signup.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {useNavigate, Link} from 'react-router-dom';
+
+function Login(){
+
+    const history = useNavigate();
+    
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function  submit(e){
+        e.preventDefault();
+
+        try{
+            await axios.post("http://localhost:8000/signup",{
+                email,password
+            })
+            .then(res=>{
+                if(res.data="exist"){
+                    alert("User already Exist")
+                }
+
+                else if(res.data="notexist"){
+                    history("/home",{state:{id:email}})
+                }
+            })
+            .catch(e=>{
+                alert("wrong details")
+                console.log(e);
+            })
+        }
+        catch{
+            console.log(e);
+        }
+    }
+
+    return(
+        
+        <div className="login">
+            <h1>Signup</h1>
+            <form action ="POST">
+                <input type = "email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" name="" id=""/>
+                <input type = "password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" name="" id=""/>
+            <input type="submit" onClick={submit}/>
+            </form>
+
+            <br/>
+            <p>OR</p>
+            <br/>
+
+            <link to="/">Login Page</link>
+        </div>
+    )
+}
+export default Login
